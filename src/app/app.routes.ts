216a43974db608b4de3d8b4f1b5526295c66db98
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-07-13 20:01:24 +0500

    routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'expense-tracker',
+        loadChildren: () => import("./expense-tracker/expense-tracker.module").then(m => m.ExpenseTrackerModule)
+    }
+];
---
+++ src/app/expense-tracker/expense-tracker.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExpenseTrackerRoutingModule } from './expense-tracker-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExpenseTrackerRoutingModule
+  ]
+})
+export class ExpenseTrackerModule { }
